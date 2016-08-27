fe18b52bfccbb97582cde0b5a7f836b0ff4a73a1
Beefstyles <glenwillis@gmail.com>
2016-08-27 10:39:57 +0800

    Added player movement with sprint mechanic.

---
+++ LudumDare36/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private float walkSpeed;
+    private float currentSpeed;
+    private float maxSpeed;
+    public float PlayerWalkSpeed;
+    public float PlayerSprintSpeed;
+
+	void FixedUpdate ()
+    {
+        currentSpeed = PlayerWalkSpeed;
+        maxSpeed = currentSpeed;
+        if (Input.GetButton("Fire1"))
+        {
+            currentSpeed = PlayerSprintSpeed;
+        }
+        GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal") * currentSpeed, 0.8F), Mathf.Lerp(0, Input.GetAxis("Vertical") * currentSpeed, 0.8F));
+	}
+}
