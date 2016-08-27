afab0916360a3f4688988872fbeb7f524bd8c9fd
Beefstyles <glenwillis@gmail.com>
2016-08-27 16:40:05 +0800

    Added drag to the hunting boomerang

--- LudumDare36/Assets/Scripts/HuntingBoomerang.cs
+++ LudumDare36/Assets/Scripts/HuntingBoomerang.cs
@@ -4,16 +4,37 @@ using System.Collections;
 public class HuntingBoomerang : MonoBehaviour {
 
     Rigidbody2D rb;
+    private bool isStationary;
+    Vector3 oppositeVelocity;
+    public float brakePower;
+    private float delayTimer;
 	
     void Start()
     {
+        delayTimer = 1F;
+        isStationary = false;
         rb = GetComponent<Rigidbody2D>();
         rb.AddTorque(15F);
     }
 	// Update is called once per frame
-	void Update () {
-        //transform.Rotate(Vector3.up * Time.deltaTime);
-	}
+	void Update ()
+    {
+        if(delayTimer >= 0)
+        {
+            delayTimer -= Time.deltaTime;
+        }
+
+        if (rb.velocity.x <= 0 || rb.velocity.y <= 0)
+        {
+            if (!isStationary && delayTimer <= 0)
+            {
+                rb.Sleep();
+                isStationary = true;
+            }
+            
+        }
+        
+    }
 
     void OnTriggerEnter2D(Collider2D coll)
     {
